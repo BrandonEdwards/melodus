8655ec243e2b3887466999680bf43c91b471ca6b
Brandon Edwards <brandon.edwards144@gmail.com>
2017-05-12 15:30:08 -0400

    High level pseudocode for general procedure

---
+++ simulate.py
@@ -0,0 +1,36 @@
+'''
+from src.agent import Agent
+
+nestMakingTime::Boolean
+availableNests::Int
+currentTime::Int
+breedingTime::Boolean
+foragingTime::Boolean
+
+Initiate enviroAgents
+
+for i in timeFrames:
+    for j in enviroAgents
+        if nestMakingTime == true && availableNests > 0
+            agent.attemptNest()
+        elif breedTime == true
+            if agent.isNest()
+                agent.layEgg()
+        else
+            if agent.isHumanPresence()
+                agent.flush(); nextAgentCell
+            if foragingTime == true
+                if agent.humanInAlertDistance() == true
+                    agent.forage(true)
+                else
+                    agent.forage(false)
+            else
+                if agent.chickAtNest() == true
+                    agent.rest()
+                else
+                    agent.findNearestNest()
+
+
+Output results
+
+'''
