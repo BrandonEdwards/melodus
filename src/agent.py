656dff07d93a49f172b2ae3a7acc18629eed6213
Brandon Edwards <brandon.edwards144@gmail.com>
2017-05-12 14:55:40 -0400

    Created preliminary attributes

--- src/agent.py
+++ src/agent.py
@@ -1,2 +1,10 @@
 class Agent(object)
-    
+    habitatType = 0
+    humanPresence = False
+    predatorPresence = False
+    nestInfo = None
+    chickWeight = list()
+    closestNest = 0
+
+    def __init__(self, habitatType):
+        self.habitatType = habitatType
