2eb665528f827593cf7db7689a256c54ab1e6fc3
Brandon Edwards <brandon.edwards144@gmail.com>
2017-05-12 15:22:04 -0400

    Import nest class

--- src/agent.py
+++ src/agent.py
@@ -1,3 +1,5 @@
+from .nest import Nest
+
 class Agent(object):
     habitatType = 0
     humanPresence = False
@@ -8,9 +10,14 @@ class Agent(object):
 
     def __init__(self, habitatType):
         self.habitatType = habitatType
+        self.nestInfo = Nest(4)
 
     def isHumanPresence(self):
         return humanPresence
+
+    def attemptNest(self):
+        #If probability pulled from a distribution is above a certain threshold,
+        #create a new nest
 
     #def flush(self):
     #def forage(self, reduced):
