24b33c898e7d71437c4021de396b435115142953
Brandon Edwards <brandon.edwards144@gmail.com>
2017-05-23 14:33:54 -0400

    Added some function stubs

--- src/nest.py
+++ src/nest.py
@@ -1,7 +1,9 @@
 class Nest(object):
     timeCreated = 0
+    totalEggs = 0
 
     def __init__(self, timeCreated):
         self.timeCreated = timeCreated
 
-    #def layEgg
+    def layEgg(self):
+    	self.totalEggs += 1
