4bd651ceadaaac73652de42763e027681b07fd75
Brandon Edwards <brandon.edwards144@gmail.com>
2017-05-30 14:20:41 -0400

    Added scenario class to be used to hold information about what scenario to simulate

---
+++ src/scenario.py
@@ -0,0 +1,16 @@
+class Scenario(object):
+	scenarioMap = None
+	initialAdults = 0
+	energyVector = list()
+
+	def __init__(self, scenarioMap, initialAdults, energyVector):
+		pass
+
+	def readScenario(file)
+		#readfile
+		#get map
+		#get inital adults
+		#get energy vector
+		#make new scenario
+		#return scenario
+		pass
