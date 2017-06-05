4d0900da7639822ec65f5bbe6cffa4e94e26d967
Brandon Edwards <brandon.edwards144@gmail.com>
2017-06-05 17:08:38 -0400

    Moved nesting hash function to scenario class. Pull initial adults from normal distribution

--- src/scenario.py
+++ src/scenario.py
@@ -1,8 +1,13 @@
+from .agent import Agent
+import numpy as np
+import math
+
 class Scenario(object):
 	def __init__(self):
 		self.scenarioMap = "maps/test.csv"
-		self.initialAdults = 10
+		self.initialAdults = math.ceil(np.random.normal(10,2,1))
 		self.energyVector = [0,0,0,0,0,0]
+		self.nestHabitatHash = dict()
 
 
 	def readScenario(file):
@@ -16,6 +21,16 @@ class Scenario(object):
 
 	def getMap(self):
 		return self.scenarioMap
+
+	def hashNestingHabitat(self, agentDB):
+		i = 0
+		for agent in agentDB:
+			if agent.getHabitatType() == 2:
+				self.nestHabitatHash[i] = agent.getAgentID()
+				i += 1
+
+	def isNestHabitat(self, agent):
+		return agent in self.nestHabitatHash
 
 	def getInitialAdults(self):
 		return self.initialAdults
