1fd7f55fad90c3e422d4d30853de19fb58bbbea2
Brandon Edwards <brandon.edwards144@gmail.com>
2017-06-05 15:53:55 -0400

    Added call to hashing function and importing of utilities

--- simulate.py
+++ simulate.py
@@ -2,6 +2,8 @@
 
 import numpy as np
 import sys
+import src.utilities as u
+import time as TIME
 from src.agent import Agent
 from src.scenario import Scenario
 print("Packges imported.")
@@ -12,6 +14,7 @@ scenario = Scenario(); print("Scenario created.")
 
 mapLocation = scenario.getMap()
 agentDB = Agent.createAgentDB(mapLocation); print("Agent database created.")
+nestHabitatHash = u.hashNestingHabitat(agentDB)
 
 totalAdults = scenario.getInitialAdults();
 
@@ -22,35 +25,44 @@ availableNests = int(totalAdults / 2)
 currentTime = 1
 breedingTime = True
 foragingTime = True
-
+'''
 print("Beginning simulation.")
 for time in range(1,35712):
+    print(time)
+    start_time = TIME.time()
     for agent in agentDB:
+
+        #We probably want to "inject" adults into random agents so that we don't have
+        #cycle through all the agents. But actually now that i think about it, we should
+        #hash the proper breeding areas so that we don't have to cycle through ALL of them
+        if availableNests > 0 and nestMakingTime == True:
+            availableNests = availableNests - agent.attemptNest(availableNests, time)
+
         if agent.isEmpty() == False:
-            print("Agent not empty apparently")
-            if availableNests > 0 and nestMakingTime == True:
-                availableNests = availableNests - agent.attemptNest(currentTime)
-            elif breedingTime == True:
+            if breedingTime == True:
                 if agent.isNest():
                     agent.layEgg()
-            else:
-                if agent.isHumanPresence():
-                    agent.flush();
-                    continue
-                if foragingTime == True:
-                    if agent.humanInAlertDistance() == True:
-                        agent.forage(True) #Reduced foraging = True
-                    else:
-                        agent.forage(False) #Reduced foraging = False
+         #   print("Agent not empty apparently")
+            if agent.isHumanPresence():
+                agent.flush();
+                continue
+            if foragingTime == True:
+                if agent.humanInAlertDistance() == True:
+                    agent.forage(True) #Reduced foraging = True
                 else:
-                    if agent.chickAtNest() == True:
-                        agent.rest()
-                    else:
-                        agent.findNearestNest()
+                    agent.forage(False) #Reduced foraging = False
+            else:
+                if agent.chickAtNest() == True:
+                    agent.rest()
+                else:
+                    agent.findNearestNest()
 
     #Update time frames
-    if currentTime > 8928:
-        nestMakingTime = False
 
+    if time > 200:
+        nestMakingTime = False
+        breedingTime = False
+    print(TIME.time() - start_time)
 
 #output data to files
+'''
