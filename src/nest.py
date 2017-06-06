d824f99d642b26d30614ee883edfc2f35ba6be59
Brandon Edwards <brandon.edwards144@gmail.com>
2017-06-05 22:45:19 -0400

    Added eggLayingTime list and hatch time

--- src/nest.py
+++ src/nest.py
@@ -1,9 +1,20 @@
 class Nest(object):
     timeCreated = 0
     totalEggs = 0
+    hatchTime = 0
+    eggLayingTimes = [None] * 4
 
     def __init__(self, timeCreated):
         self.timeCreated = timeCreated
+
+        for i in range(0,4):
+        	self.eggLayingTimes[i] = (timeCreated + ((i+1) * 576))
+
+        self.hatchTime = self.eggLayingTimes[3] + (27 * 288)
+
+        print(self.eggLayingTimes)
+        print(self.hatchTime)
+        
 
     def layEgg(self):
     	self.totalEggs += 1
