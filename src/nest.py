693087368858d5c79970a4868d4804416b752d1f
Brandon Edwards <brandon.edwards144@gmail.com>
2017-06-06 11:31:05 -0400

    Moved class variables to inside __init__. Apparently that's important

--- src/nest.py
+++ src/nest.py
@@ -1,26 +1,20 @@
 class Nest(object):
-    timeCreated = 0
-    totalEggs = 0
-    hatchTime = 0
-    eggLayingTimes = [None] * 4
+	def __init__(self, timeCreated):
+		self.timeCreated = timeCreated
+		self.eggLayingTimes = list()
+		self.hatchTime = None
+		self.totalEggs = 0
 
-    def __init__(self, timeCreated):
-        self.timeCreated = timeCreated
+		for i in range(0,4):
+			self.eggLayingTimes.append(timeCreated + ((i+1) * 576))
 
-        for i in range(0,4):
-        	self.eggLayingTimes[i] = (timeCreated + ((i+1) * 576))
+		self.hatchTime = self.eggLayingTimes[3] + (27 * 288)
 
-        self.hatchTime = self.eggLayingTimes[3] + (27 * 288)
-
-        print(self.eggLayingTimes)
-        print(self.hatchTime)
-        
-
-    def layEgg(self, time):
-    	if time == self.eggLayingTimes[0]:
-    		self.totalEggs += 1
-    		del self.eggLayingTimes[0]
-    		print(self.eggLayingTimes)
-    		return 1
-    	else:
-    		return 0
+	def layEgg(self, time):
+		if time == self.eggLayingTimes[0]:
+			self.totalEggs += 1
+			del self.eggLayingTimes[0]
+			print(self.eggLayingTimes)
+			return 1
+		else:
+			return 0
