d17cb457be7969f7546d85fe0bfcd5676b89544f
Brandon Edwards <brandon.edwards144@gmail.com>
2017-07-05 15:09:51 -0400

    Creates new directories and outputs chick weights

--- src/fileio.py
+++ src/fileio.py
@@ -1,16 +1,45 @@
 import numpy as np
 import pandas as pd
+import os
+import time
 
 class IO(object):
 	def __init__(self):
 		self.chickWeightData = pd.DataFrame(np.zeros(0, dtype=[('Day', int), ('Num.Chicks', int),
-    							('Weight', str), ('Mean.Weight', float)]))
+								('Weight', str), ('Mean.Weight', float)]))
+
+	def createOutputDir(self):
+		self.ensureResultsDir()
+		dateDir = self.ensureDateDir()
+
+		runNum = str(len(os.listdir(dateDir)))
+		outputDir = dateDir + "/run" + runNum + "/"
+
+		os.makedirs(outputDir)
+
+		return outputDir		
+	def ensureDateDir(self):
+		directory = "results/" + time.strftime("%d-%m-%Y")
+
+		if not os.path.exists(directory):
+			os.makedirs(directory)
+
+		return directory
+
+	def ensureResultsDir(self):
+		if not os.path.exists("results/"):
+			os.makedirs("results/")
+
+	def outputResults(self):
+		directory = self.createOutputDir()
+
+		self.chickWeightData.to_csv(directory + "chickWeights.csv", index = False)
 
 	def updateChickWeight(self, day, totalChickWeights):
 		weightString = ",".join(str(i) for i in totalChickWeights)
 
-        # Append information to chickWeight Data frame (hopefully)
-        chickWeightData = chickWeightData.append({'Day':day, 'Num.Chicks':len(totalChickWeights),
-            'Weight':weightString,
-            'Mean.Weight':(sum(totalChickWeights)/len(totalChickWeights))}, 
-            ignore_index=True)
+		# Append information to chickWeight Data frame (hopefully)
+		self.chickWeightData = self.chickWeightData.append({'Day':day, 'Num.Chicks':len(totalChickWeights),
+			'Weight':weightString,
+			'Mean.Weight':(sum(totalChickWeights)/len(totalChickWeights))}, 
+			ignore_index=True)
