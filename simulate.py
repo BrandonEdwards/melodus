2a1f6eaf278f55c1d3b856b77837ff54dcde59d6
Brandon Edwards <brandon.edwards144@gmail.com>
2017-07-05 15:10:07 -0400

    Make calls to update and output chick weights

--- simulate.py
+++ simulate.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 
 import numpy as np
+import pandas as pd
 import sys
 import csv
 import src.utilities as util
 import time as TIME
 from src.agent import Agent
 from src.scenario import Scenario
-thread = 1#sys.argv[1]
+from src.fileio import IO
+
+thread = sys.argv[1]
 print("Packges imported - thread ", thread)
+
+io = IO()
 
 #implement this later
 #scenario = util.readScenario(str(sys.argv[1]))
@@ -22,8 +27,17 @@ scenario.hashNestingHabitat(agentDB); print("Nesting habitat hash created.")
 
 totalAdults = scenario.getInitialAdults(); print("Initial adults = ", totalAdults)
 
+#Create empty dataframe to store chick weight data. Probably will move this to different file
+chickWeightData = pd.DataFrame(np.zeros(0, dtype=[('Day', int), ('Num.Chicks', int),
+    ('Weight', str), ('Mean.Weight', float)]))
+
 #Change these later
 elapsedTime = 0.0
+alertTime = 0.0
+forageTime = 0.0
+moveTime = 0.0
+restTime = 0.0
+nearestNestTime = 0.0
 nestMakingTime = True
 #assume 50/50 split of males and females
 availableNests = int(totalAdults / 2)
@@ -35,31 +49,53 @@ foragingTime = False
 nextActiveAgents = list()
 
 print("Beginning simulation.")
-for time in range(0,8640):#35712):
+for time in range(0,9000):#35712):
     if time % 288 == 0:
         totalChickWeights = list()
         output = list()
 
-        day = time / 288
+        day = (time / 288)
+        # Delete these later
         print("Current day is ", day)
         print(thread,"- It took ", elapsedTime, " seconds for the previous day.")
+        """
+        print("Alert time: ", alertTime)
+        print("Forage time: ", forageTime)
+        print("Move time: ", moveTime)
+        print("Rest Time: ", restTime)
+        print("Nearest Nest Time: ", nearestNestTime)
         print("Current chick weights are as follows:")
-
+        """
         for agent in nextActiveAgents:
             print(agent.chickWeight)
             totalChickWeights.extend(agent.chickWeight)
 
         print()
+        alertTime = 0.0
+        forageTime = 0.0
+        moveTime = 0.0
+        restTime = 0.0
+        nearestNestTime = 0.0
         elapsedTime = 0.0
         if (time > 0):
-            output.append(day)
-            output.append(len(totalChickWeights))
-            output.extend(totalChickWeights)
-            output.append(sum(totalChickWeights) / len(totalChickWeights))
-
-            with open("testResultsNoHumans.csv", "a") as f:
-                w = csv.writer(f)
-                w.writerow(output)
+            # output.append(day)
+            # output.append(len(totalChickWeights))
+            # output.extend(totalChickWeights)
+            # output.append(sum(totalChickWeights) / len(totalChickWeights))
+            '''
+            weightString = ",".join(str(i) for i in totalChickWeights)
+            # Append information to chickWeight Data frame (hopefully)
+            chickWeightData = chickWeightData.append({'Day':day, 'Num.Chicks':len(totalChickWeights),
+                'Weight':weightString,
+                'Mean.Weight':(sum(totalChickWeights)/len(totalChickWeights))}, 
+                ignore_index=True)
+            '''
+            io.updateChickWeight(day, totalChickWeights)
+            #print(chickWeightData)
+            #filename = thread + "testResultsNoHumans" + "10" + ".csv"
+            #with open(filename, "a") as f:
+            #    w = csv.writer(f)
+            #    w.writerow(output)
 
     start_time = TIME.time()
 
@@ -97,18 +133,32 @@ for time in range(0,8640):#35712):
                     scenario.getMapWidth()));
                 continue
             if foragingTime == True:
+                t = TIME.time()
                 alert = agent.humanInAlertDistance(agentDB, IDToAgent, scenario.getHabitatVector(),
                         scenario.getMapWidth())
+                alertTime += (TIME.time() - t)
+
+                t = TIME.time()
                 agent.forage(scenario.getEnergyVector(), alert)
+                forageTime += (TIME.time() - t)
+
+                t = TIME.time()
                 nextActiveAgents.append(agent.move(agentDB, IDToAgent, scenario.getHabitatVector(), 
                     scenario.getEnergyVector(), scenario.getMapWidth()))
+                moveTime += (TIME.time() - t)
             else:
+                agent.rest()
+                nextActiveAgents.append(agent)
+                """
                 if agent.chickAtNest() == True:
+                    t = TIME.time()
                     agent.rest()
                     nextActiveAgents.append(agent)
+                    restTime += (TIME.time() - t)
                 else:
+                    t = TIME.time()
                     nextActiveAgents.append(agent.findNearestNest(agentDB, IDToAgent, scenario.getHabitatVector(), scenario.getMapWidth()))
-
+                    nearestNestTime += (TIME.time() - t)"""
     if nestMakingTime == True and time > 9000:
         nestMakingTime = False; print("Nest making time has ended.")
     if breedingTime == True and time > 20000:
@@ -117,4 +167,4 @@ for time in range(0,8640):#35712):
         foragingTime = not foragingTime
     elapsedTime += (TIME.time() - start_time)
 
-#output data to files
+io.outputResults()
